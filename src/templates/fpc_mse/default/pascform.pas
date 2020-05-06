c3028de6619891020ea3141d9af5c9460e61631a
fredvs <fiens@hotmail.com>
2020-05-06 22:15:35 +0200

    dded msegraphics,msegraphutils when creating a new project.

--- src/templates/fpc_mse/default/pascform.pas
+++ src/templates/fpc_mse/default/pascform.pas
@@ -2,9 +2,8 @@ unit ${%UNITNAME%};
 {$ifdef FPC}{$mode objfpc}{$h+}{$endif}
 interface
 uses
- msetypes,mseglob,mseguiglob,mseguiintf,mseapplication,msestat,msemenus,msegui,
- msegraphics,msegraphutils,mseevent,mseclasses,msewidgets,mseforms,
- msepascalscript;
+ msetypes, mseglob, mseguiglob, mseguiintf, mseapplication, msestat, msemenus, msegui,
+ msegraphics, msegraphutils, mseevent, mseclasses, msewidgets, mseforms, msepascalscript;
 type
  t${%FORMNAME%} = class(tpascform)
  end;
